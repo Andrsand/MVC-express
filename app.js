8f557a1b03695cfe14e7f927c74983a1dd19dc88
andrsand <angorko@list.ru>
2023-04-10 21:57:12 +0300

    moving routes to a separate folder and installing hbs

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 const express = require("express");
 const app = express();
-const userController = require("./controllers/userController.js");
-const homeController = require("./controllers/homeController.js");
+const userRouter = require("./routes/userRouter.js");
+const homeRouter = require("./routes/homeRouter.js");
 
-// определяем роутеры
-const userRouter = express.Router();  // для адресов с "/users"
-const homeRouter = express.Router();
-
-// определяем маршруты и их обработчики внутри роутера userRouter
-userRouter.use("/create", userController.addUser);
-userRouter.use("/", userController.getUsers);
-app.use("/users", userRouter);
-
-// определяем маршруты и их обработчики внутри роутера homeRouter
-homeRouter.get("/about", homeController.about);
-homeRouter.get("/", homeController.index);
+app.use("/users", userRouter);;
 app.use("/", homeRouter);
 
 app.use(function (req, res, next) {
